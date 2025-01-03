edc08dfb39f535e96bfff76bf982b13266fa1e22
Alexey-Ayupov <Alexey-Ayupov@github.com>
2025-01-03 18:41:52 +0100

    Try to use interact session 5.4

--- images/windows/scripts/build/Configure-runneradmin.ps1
+++ images/windows/scripts/build/Configure-runneradmin.ps1
@@ -2,15 +2,18 @@
 function Start-AgentAsInteractiveUser([string]$TaskName, [string]$Execute, [PSCredential]$Credential, [int]$MaxAttemptsCount, [int]$DelayBeforeCheckAgentStatusSeconds) {
     $startTime = [DateTime]::Now
                 $userName = $Credential.GetNetworkCredential().UserName
+                write-host $userName
                 $userSid = (New-Object System.Security.Principal.NTAccount($userName)).Translate([System.Security.Principal.SecurityIdentifier]).Value
                 $cimSession = New-CimSession -ComputerName $env:COMPUTERNAME -Credential $Credential
                 $action =  New-ScheduledTaskAction -Execute $Execute -CimSession $cimSession
+                Write-Host "Registering task with action - $action"
                 $settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit ([TimeSpan]::FromSeconds(0)) -DontStopOnIdleEnd -MultipleInstances IgnoreNew  -CimSession $cimSession
                 $principal = New-ScheduledTaskPrincipal -UserId $userSid -RunLevel Highest -LogonType Interactive -CimSession $cimSession
                 Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false -ErrorAction SilentlyContinue -CimSession $cimSession
                 Register-ScheduledTask -TaskName $TaskName -Action $action -Principal $principal -Settings $settings -CimSession $cimSession
                 $attemptIndex = 1
                 while ($attemptIndex -le $MaxAttemptsCount) {
+                    Write-Host "------------in------------"
                     Write-Host "Starting scheduled task with agent, attempt $attemptIndex"
                     & { $ErrorActionPreference = 'Continue'; $out = query user $userName 2>&1; Write-Host $out }
                     Start-ScheduledTask -TaskName $TaskName -CimSession $cimSession
@@ -50,4 +53,7 @@ $userPasswordSecure = ConvertTo-SecureString $userPassword -AsPlainText -Force
 $credentials = [System.Management.Automation.PSCredential]::new("$env:COMPUTERNAME\$userName", $userPasswordSecure)
 
 Set-WSManQuickConfig -Force -SkipNetworkProfileCheck
-Start-AgentAsInteractiveUser -TaskName 'Runner' -Execute "${env:IMAGE_FOLDER}\run.cmd" -Credential $credentials
+Start-AgentAsInteractiveUser -TaskName 'Runner' -Execute "${env:IMAGE_FOLDER}\run.cmd" -Credential $credentials -MaxAttemptsCount 5 -DelayBeforeCheckAgentStatusSeconds 10
+
+$RegistryPath = 'HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon'
+Get-ItemProperty -Path $RegistryPath
